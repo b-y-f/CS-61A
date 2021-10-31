94c1dea1cfdbc6a2ed7ee9dbc96f3df7616825f4
yifan <bbbbb8bbbbb@gmail.com>
2021-10-31 22:14:21 +1300

    save

--- discs/disc04.py
+++ discs/disc04.py
@@ -1,11 +1,62 @@
-# Lists
+# new
+def count_stair_ways(n):
+    """Returns the number of ways to climb up a flight of
+    n stairs, moving either 1 step or 2 steps at a time.
+    >>> count_stair_ways(4)
+    5
+    """
+    "*** YOUR CODE HERE ***"
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    else:
+        return count_stair_ways(n-1) + count_stair_ways(n-2)
+
+def count_k(n, k):
+    """ Counts the number of paths up a flight of n stairs
+    when taking up to and including k steps at a time.
+    >>> count_k(3, 3) # 3, 2 + 1, 1 + 2, 1 + 1 + 1
+    4
+    >>> count_k(4, 4)
+    8
+    >>> count_k(10, 3)
+    274
+    >>> count_k(300, 1) # Only one step at a time
+    1
+    """
+    "*** YOUR CODE HERE ***"
+
+
 def even_weighted(s):
     """
     >>> x = [1, 2, 3, 4, 5, 6]
     >>> even_weighted(x)
     [0, 6, 20]
     """
-    return [i*j for i,j in enumerate(s) if i%2==0]
+    return [index*s[index] for index in range(len(s)) if index % 2 == 0]
+
+
+def max_product(s):
+    """Return the maximum product that can be formed using
+    non-consecutive elements of s.
+    >>> max_product([10,3,1,9,2]) # 10 * 9
+    90
+    >>> max_product([5,10,5,10,5]) # 5 * 5 * 5
+    125
+    >>> max_product([1,2,3,4,5,6]) # 2*4*6
+    48
+    >>> max_product([])
+    1
+    """
+    if s == []:
+        return 1
+    else:
+        right, prod = max_product(s[1:]), s[0] * max_product(s[2:])
+        return max(prod, right)
+
+
+
 
 
 def closest_number(nums, target):
@@ -18,24 +69,6 @@ def closest_number(nums, target):
     23
     """
     return min(nums,key=lambda x:abs(x-target))
-
-
-def max_product(s):
-    """Return the maximum product that can be formed using non-consecutive
-    elements of s.
-
-    >>> max_product([10,3,1,9,2]) # 10 * 9
-    90
-    >>> max_product([5,10,5,10,5]) # 5 * 5 * 5
-    125
-    >>> max_product([])
-    1
-    """
-    # using **nonconsecutive** elements of the list
-    if len(s) == 0:
-        return 1
-    else:
-        return max(max_product(s[1:]),s[0]*max_product(s[2:]))
 
 
 # Dict
