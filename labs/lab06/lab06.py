aa510e1d358e6c4c30f172cad371e144c9dcc41c
yifan <bbbbb8bbbbb@gmail.com>
2021-11-02 00:34:46 +1300

    new lab

--- labs/lab06/lab06.py
+++ labs/lab06/lab06.py
@@ -1,240 +1,78 @@
-def insert_into_all(item, nested_list):
-    """Return a new list consisting of all the lists in nested_list,
-    but with item added to the front of each. You can assuming that
-     nested_list is a list of lists.
+HW_SOURCE_FILE = __file__
 
-    >>> nl = [[], [1, 2], [3]]
-    >>> insert_into_all(0, nl)
-    [[0], [0, 1, 2], [0, 3]]
+
+def insert_items(lst, entry, elem):
+    """Inserts elem into lst after each occurence of entry and then returns lst.
+
+    >>> test_lst = [1, 5, 8, 5, 2, 3]
+    >>> new_lst = insert_items(test_lst, 5, 7)
+    >>> new_lst
+    [1, 5, 7, 8, 5, 7, 2, 3]
+    >>> double_lst = [1, 2, 1, 2, 3, 3]
+    >>> double_lst = insert_items(double_lst, 3, 4)
+    >>> double_lst
+    [1, 2, 1, 2, 3, 4, 3, 4]
+    >>> large_lst = [1, 4, 8]
+    >>> large_lst2 = insert_items(large_lst, 4, 4)
+    >>> large_lst2
+    [1, 4, 4, 8]
+    >>> large_lst3 = insert_items(large_lst2, 4, 6)
+    >>> large_lst3
+    [1, 4, 6, 4, 6, 8]
+    >>> large_lst3 is large_lst
+    True
+    >>> # Ban creating new lists
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'insert_items',
+    ...       ['List', 'ListComp', 'Slice'])
+    True
     """
     "*** YOUR CODE HERE ***"
 
 
-def subseqs(s):
-    """Return a nested list (a list of lists) of all subsequences of S.
-    The subsequences can appear in any order. You can assume S is a list.
+def count_occurrences(t, n, x):
+    """Return the number of times that x appears in the first n elements of iterator t.
 
-    >>> seqs = subseqs([1, 2, 3])
-    >>> sorted(seqs)
-    [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]]
-    >>> subseqs([])
-    [[]]
-    """
-    if ________________:
-        ________________
-    else:
-        ________________
-        ________________
-
-
-def non_decrease_subseqs(s):
-    """Assuming that S is a list, return a nested list of all subsequences
-    of S (a list of lists) for which the elements of the subsequence
-    are strictly nondecreasing. The subsequences can appear in any order.
-
-    >>> seqs = non_decrease_subseqs([1, 3, 2])
-    >>> sorted(seqs)
-    [[], [1], [1, 2], [1, 3], [2], [3]]
-    >>> non_decrease_subseqs([])
-    [[]]
-    >>> seqs2 = non_decrease_subseqs([1, 1, 2])
-    >>> sorted(seqs2)
-    [[], [1], [1], [1, 1], [1, 1, 2], [1, 2], [1, 2], [2]]
-    """
-    def subseq_helper(s, prev):
-        if not s:
-            return ____________________
-        elif s[0] < prev:
-            return ____________________
-        else:
-            a = ______________________
-            b = ______________________
-            return insert_into_all(________, ______________) + ________________
-    return subseq_helper(____, ____)
-
-
-def trade(first, second):
-    """Exchange the smallest prefixes of first and second that have equal sum.
-
-    >>> a = [1, 1, 3, 2, 1, 1, 4]
-    >>> b = [4, 3, 2, 7]
-    >>> trade(a, b) # Trades 1+1+3+2=7 for 4+3=7
-    'Deal!'
-    >>> a
-    [4, 3, 1, 1, 4]
-    >>> b
-    [1, 1, 3, 2, 2, 7]
-    >>> c = [3, 3, 2, 4, 1]
-    >>> trade(b, c)
-    'No deal!'
-    >>> b
-    [1, 1, 3, 2, 2, 7]
-    >>> c
-    [3, 3, 2, 4, 1]
-    >>> trade(a, c)
-    'Deal!'
-    >>> a
-    [3, 3, 2, 1, 4]
-    >>> b
-    [1, 1, 3, 2, 2, 7]
-    >>> c
-    [4, 3, 1, 4, 1]
-    """
-    m, n = 1, 1
-
-    equal_prefix = lambda: ______________________
-    while _______________________________:
-        if __________________:
-            m += 1
-        else:
-            n += 1
-
-    if equal_prefix():
-        first[:m], second[:n] = second[:n], first[:m]
-        return 'Deal!'
-    else:
-        return 'No deal!'
-
-
-def card(n):
-    """Return the playing card numeral as a string for a positive n <= 13."""
-    assert type(n) == int and n > 0 and n <= 13, "Bad card n"
-    specials = {1: 'A', 11: 'J', 12: 'Q', 13: 'K'}
-    return specials.get(n, str(n))
-
-
-def shuffle(cards):
-    """Return a shuffled list that interleaves the two halves of cards.
-
-    >>> shuffle(range(6))
-    [0, 3, 1, 4, 2, 5]
-    >>> suits = ['♡', '♢', '♤', '♧']
-    >>> cards = [card(n) + suit for n in range(1,14) for suit in suits]
-    >>> cards[:12]
-    ['A♡', 'A♢', 'A♤', 'A♧', '2♡', '2♢', '2♤', '2♧', '3♡', '3♢', '3♤', '3♧']
-    >>> cards[26:30]
-    ['7♤', '7♧', '8♡', '8♢']
-    >>> shuffle(cards)[:12]
-    ['A♡', '7♤', 'A♢', '7♧', 'A♤', '8♡', 'A♧', '8♢', '2♡', '8♤', '2♢', '8♧']
-    >>> shuffle(shuffle(cards))[:12]
-    ['A♡', '4♢', '7♤', '10♧', 'A♢', '4♤', '7♧', 'J♡', 'A♤', '4♧', '8♡', 'J♢']
-    >>> cards[:12]  # Should not be changed
-    ['A♡', 'A♢', 'A♤', 'A♧', '2♡', '2♢', '2♤', '2♧', '3♡', '3♢', '3♤', '3♧']
-    """
-    assert len(cards) % 2 == 0, 'len(cards) must be even'
-    half = _______________
-    shuffled = []
-    for i in _____________:
-        _________________
-        _________________
-    return shuffled
-
-
-def add_trees(t1, t2):
-    """
-    >>> numbers = tree(1,
-    ...                [tree(2,
-    ...                      [tree(3),
-    ...                       tree(4)]),
-    ...                 tree(5,
-    ...                      [tree(6,
-    ...                            [tree(7)]),
-    ...                       tree(8)])])
-    >>> print_tree(add_trees(numbers, numbers))
+    >>> s = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
+    >>> count_occurrences(s, 10, 9)
+    3
+    >>> s2 = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
+    >>> count_occurrences(s2, 3, 10)
     2
-      4
-        6
-        8
-      10
-        12
-          14
-        16
-    >>> print_tree(add_trees(tree(2), tree(3, [tree(4), tree(5)])))
-    5
-      4
-      5
-    >>> print_tree(add_trees(tree(2, [tree(3)]), tree(2, [tree(3), tree(4)])))
-    4
-      6
-      4
-    >>> print_tree(add_trees(tree(2, [tree(3, [tree(4), tree(5)])]), \
-    tree(2, [tree(3, [tree(4)]), tree(5)])))
-    4
-      6
-        8
-        5
-      5
+    >>> s = iter([3, 2, 2, 2, 1, 2, 1, 4, 4, 5, 5, 5])
+    >>> count_occurrences(s, 1, 3)
+    1
+    >>> count_occurrences(s, 4, 2)
+    3
+    >>> next(s)
+    2
+    >>> s2 = iter([4, 1, 6, 6, 7, 7, 8, 8, 2, 2, 2, 5])
+    >>> count_occurrences(s2, 6, 6)
+    2
     """
     "*** YOUR CODE HERE ***"
 
 
-# Tree ADT
+def repeated(t, k):
+    """Return the first value in iterator T that appears K times in a row.
+    Iterate through the items such that if the same iterator is passed into
+    the function twice, it continues in the second call at the point it left
+    off in the first.
 
-def tree(label, branches=[]):
-    """Construct a tree with the given label value and a list of branches."""
-    for branch in branches:
-        assert is_tree(branch), 'branches must be trees'
-    return [label] + list(branches)
-
-
-def label(tree):
-    """Return the label value of a tree."""
-    return tree[0]
-
-
-def branches(tree):
-    """Return the list of branches of the given tree."""
-    return tree[1:]
-
-
-def is_tree(tree):
-    """Returns True if the given tree is a tree, and False otherwise."""
-    if type(tree) != list or len(tree) < 1:
-        return False
-    for branch in branches(tree):
-        if not is_tree(branch):
-            return False
-    return True
-
-
-def is_leaf(tree):
-    """Returns True if the given tree's list of branches is empty, and False
-    otherwise.
-    """
-    return not branches(tree)
-
-
-def print_tree(t, indent=0):
-    """Print a representation of this tree in which each node is
-    indented by two spaces times its depth from the root.
-
-    >>> print_tree(tree(1))
-    1
-    >>> print_tree(tree(1, [tree(2)]))
-    1
-      2
-    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
-    >>> print_tree(numbers)
-    1
-      2
-      3
-        4
-        5
-      6
-        7
-    """
-    print('  ' * indent + str(label(t)))
-    for b in branches(t):
-        print_tree(b, indent + 1)
-
-
-def copy_tree(t):
-    """Returns a copy of t. Only for testing purposes.
-
-    >>> t = tree(5)
-    >>> copy = copy_tree(t)
-    >>> t = tree(6)
-    >>> print_tree(copy)
+    >>> s = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
+    >>> repeated(s, 2)
+    9
+    >>> s2 = iter([10, 9, 10, 9, 9, 10, 8, 8, 8, 7])
+    >>> repeated(s2, 3)
+    8
+    >>> s = iter([3, 2, 2, 2, 1, 2, 1, 4, 4, 5, 5, 5])
+    >>> repeated(s, 3)
+    2
+    >>> repeated(s, 3)
     5
+    >>> s2 = iter([4, 1, 6, 6, 7, 7, 8, 8, 2, 2, 2, 5])
+    >>> repeated(s2, 3)
+    2
     """
-    return tree(label(t), [copy_tree(b) for b in branches(t)])
+    assert k > 1
+    "*** YOUR CODE HERE ***"
