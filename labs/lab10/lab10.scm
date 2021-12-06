db02f35c63d98a9952ae7de7cd2af5b5aed92881
Yifan Bao <bbbbb8bbbbb@gmail.com>
2021-12-06 23:51:56 +1300

    lab 10 ok

--- labs/lab10/lab10.scm
+++ labs/lab10/lab10.scm
@@ -1,9 +1,29 @@
-(define (over-or-under num1 num2) 'YOUR-CODE-HERE)
+(define (over-or-under num1 num2) 
+    (cond
+        ((< num1 num2) -1)
+        ((= num1 num2) 0)
+        ((> num1 num2) 1)
+    )   
+)
 
-(define (make-adder num) 'YOUR-CODE-HERE)
+(define (make-adder num) 
+    (lambda (inc) (+ num inc) )
+)
 
-(define (composed f g) 'YOUR-CODE-HERE)
+(define (composed f g) 
+    (lambda (x) (f(g x)))
+)
 
-(define lst 'YOUR-CODE-HERE)
+(define lst 
+    (cons (cons 1 nil) 
+        (cons 2 
+            (cons (cons 3 (cons 4 nil))
+                (cons 5 nil)
+            )
+            
+        )
+    )
+)
 
+; TODO
 (define (remove item lst) 'YOUR-CODE-HERE)
